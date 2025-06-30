1cc63772ee59c5ad81374104bcf5c8fa619b065a
John Marsing <JohnMarsing@outlook.com>
2025-06-30 10:27:21 -0700

    Added sukkot crap to appsettings.prod, went back to using IsRegistrationClosed

--- LivingMessiah/Settings/SukkotSettings.cs
+++ LivingMessiah/Settings/SukkotSettings.cs
@@ -3,8 +3,7 @@
 public class SukkotSettings
 {
 	public bool SukkotIsOpen { get; set; }
-	//public bool IsRegistrationClosed { get; set; }
-	public bool SukkotRegistrationIsClosed { get; set; }
+	public bool IsRegistrationClosed { get; set; }
 	public string? StripeBuyButtonId { get; set; }
 	public string? StripePublishableKey { get; set; }
 }
