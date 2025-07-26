db4f5428705336c7e7b3c057423cfaa09b78017d
John Marsing <JohnMarsing@outlook.com>
2025-07-26 11:43:37 -0700

    data rub gen 41:38, first attempt at Moon API

---
+++ LivingMessiah/Features/LunarMonths/UsnoRecord.cs
@@ -0,0 +1,9 @@
+﻿
+namespace LivingMessiah.Features.LunarMonths;
+
+public record UsnoRecord (string Moonrise, string Moonset, string MoonPhase, string FracIllum);
+
+/*
+USNO: (United States Naval Observatory
+Ignore Spelling: Usno
+*/
